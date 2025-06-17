c3320c68756500546b267f97a4d6a17ba83a6b6b
idkana <ifidkidknow@gmail.com>
2025-06-18 05:27:22 +0800

    remove naersk

--- modules/shell/starship/starship.nix
+++ modules/shell/starship/starship.nix
@@ -1,4 +1,4 @@
-{ inputs, self, ... }:
+{ self, ... }:
 {
   flake.modules.homeManager.common =
     { pkgs, lib, ... }:
@@ -10,35 +10,39 @@
       xdg.dataFile."nushell/vendor/autoload/starship.nu".source = "${pkgs.runCommand "starship.nu" { } ''
         ${lib.getExe pkgs.starship} init nu > "$out"
       ''}";
-      xdg.configFile."starship.toml".text = ''
-        ${lib.readFile ./starship.toml}
 
-        [custom.jj]
-        command = "${lib.getExe starship-jj} --ignore-working-copy starship prompt"
-        format = "[$symbol](blue bold) $output "
-        symbol = "󱗆 "
-        when = "${lib.getExe pkgs.jujutsu} root --ignore-working-copy"
-      '';
+      xdg.configFile."starship.toml".source = pkgs.concatText "starship.toml" [
+        ./starship.toml
+        (pkgs.writeText "starship-custom-jj.toml" ''
+          [custom.jj]
+          command = "${lib.getExe starship-jj} --ignore-working-copy starship prompt"
+          format = "[$symbol](blue bold) $output "
+          symbol = "󱗆 "
+          when = "${lib.getExe pkgs.jujutsu} root --ignore-working-copy"
+        '')
+      ];
       xdg.configFile."starship-jj/starship-jj.toml".source = ./starship-jj.toml;
     };
 
   perSystem =
     { pkgs, ... }:
     {
-      packages.starship-jj =
-        let
-          version = "0.3.2";
-          src = pkgs.fetchFromGitLab {
-            owner = "lanastara_foss";
-            repo = "starship-jj";
-            tag = version;
-            hash = "sha256-+wATQ3uXxUFFQt/Fz8PKZ7NmPzaNPfjWH/gfMXHryO4=";
-          };
-          naersk' = pkgs.callPackage inputs.naersk { };
-        in
-        naersk'.buildPackage {
-          src = src;
-          meta.mainProgram = "starship-jj";
+      packages.starship-jj = pkgs.rustPlatform.buildRustPackage (finalAttrs: {
+        pname = "starship-jj";
+        version = "0.3.2";
+        src = pkgs.fetchFromGitLab {
+          owner = "lanastara_foss";
+          repo = "starship-jj";
+          tag = finalAttrs.version;
+          hash = "sha256-+wATQ3uXxUFFQt/Fz8PKZ7NmPzaNPfjWH/gfMXHryO4=";
         };
+
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [ pkgs.openssl ];
+
+        cargoHash = "sha256-to+3CJc0em/sEhHj3j4tT8ucqxypz0/JOohpzraJREg=";
+
+        meta.mainProgram = "starship-jj";
+      });
     };
 }
