3e40a8386e0c05a0d7b88bbfc37220f8bf51a35a
idkana <ifidkidknow@gmail.com>
2025-07-07 04:17:00 +0800

    configure IME

--- modules/hosts/sakiko/kde.nix
+++ modules/hosts/sakiko/kde.nix
@@ -1,8 +1,24 @@
 {
-  flake.modules.nixos."hosts/sakiko" = {
-    services.xserver.enable = true;
-    services.displayManager.sddm.enable = true;
-    services.displayManager.sddm.wayland.enable = true;
-    services.desktopManager.plasma6.enable = true;
-  };
+  flake.modules.nixos."hosts/sakiko" =
+    { pkgs, ... }:
+    {
+      services.xserver.enable = true;
+      services.displayManager.sddm.enable = true;
+      services.displayManager.sddm.wayland.enable = true;
+      services.desktopManager.plasma6.enable = true;
+
+      i18n.inputMethod = {
+        enable = true;
+        type = "fcitx5";
+        fcitx5.waylandFrontend = true;
+        fcitx5.plasma6Support = true;
+        fcitx5.addons = with pkgs; [
+          kdePackages.fcitx5-chinese-addons
+          fcitx5-rime
+          fcitx5-gtk
+          kdePackages.fcitx5-qt
+          fcitx5-mozc
+        ];
+      };
+    };
 }
