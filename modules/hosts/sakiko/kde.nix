fa64b49b1b031a8336e7dd32f44cbefa7b5b2d51
idkana <ifidkidknow@gmail.com>
2025-07-07 10:55:14 +0800

    remove plasma-browser-integration (sakiko)

--- modules/hosts/sakiko/kde.nix
+++ modules/hosts/sakiko/kde.nix
@@ -7,6 +7,10 @@
       services.displayManager.sddm.wayland.enable = true;
       services.desktopManager.plasma6.enable = true;
 
+      environment.plasma6.excludePackages = with pkgs.kdePackages; [
+        plasma-browser-integration
+      ];
+
       i18n.inputMethod = {
         enable = true;
         type = "fcitx5";
