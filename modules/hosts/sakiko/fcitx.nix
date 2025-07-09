9952657690169baf461f9633256b5ea25af4fdea
idkana <ifidkidknow@gmail.com>
2025-07-09 16:03:20 +0800

    factor: fcitx

---
+++ modules/hosts/sakiko/fcitx.nix
@@ -0,0 +1,19 @@
+{
+  flake.modules.nixos."hosts/sakiko" =
+    { pkgs, ... }:
+    {
+      i18n.inputMethod = {
+        enable = true;
+        type = "fcitx5";
+        # fcitx5.waylandFrontend = true;
+        fcitx5.addons = with pkgs; [
+          kdePackages.fcitx5-chinese-addons
+          fcitx5-rime
+          fcitx5-gtk
+          kdePackages.fcitx5-qt
+          fcitx5-mozc
+          fcitx5-mellow-themes
+        ];
+      };
+    };
+}
--- modules/hosts/sakiko/kde.nix
+++ modules/hosts/sakiko/kde.nix
@@ -11,19 +11,6 @@
         plasma-browser-integration
       ];
 
-      i18n.inputMethod = {
-        enable = true;
-        type = "fcitx5";
-        fcitx5.waylandFrontend = true;
-        fcitx5.plasma6Support = true;
-        fcitx5.addons = with pkgs; [
-          kdePackages.fcitx5-chinese-addons
-          fcitx5-rime
-          fcitx5-gtk
-          kdePackages.fcitx5-qt
-          fcitx5-mozc
-          fcitx5-mellow-themes
-        ];
-      };
+      i18n.inputMethod.fcitx5.plasma6Support = true;
     };
 }
