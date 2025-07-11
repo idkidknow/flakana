9e6f2528bf5c3dc4f91d64d41a7d058d7335e83b
idkana <ifidkidknow@gmail.com>
2025-07-11 14:17:18 +0800

    add metacubexd (sakiko)

--- modules/hosts/sakiko/mihomo.nix
+++ modules/hosts/sakiko/mihomo.nix
@@ -1,10 +1,11 @@
 {
   flake.modules.nixos."hosts/sakiko" =
-    { ... }:
+    { pkgs, ... }:
     {
       services.mihomo = {
         enable = true;
         configFile = "/etc/mihomo/config.yaml";
+        webui = pkgs.metacubexd;
         tunMode = true;
       };
     };
