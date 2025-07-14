41f1c656950fc2b60b8b188739108d3a875b1963
idkana <ifidkidknow@gmail.com>
2025-07-14 08:36:46 +0800

    fix: IME not working in qq

--- modules/hosts/sakiko/idkana/im.nix
+++ modules/hosts/sakiko/idkana/im.nix
@@ -4,7 +4,10 @@
     {
       nixpkgs.config.allowUnfree = true;
       home.packages = with pkgs; [
-        qq
+        (qq.override {
+          # Fix the issue that the IME does not work when NIXOS_OZONE_WL=1
+          commandLineArgs = "--wayland-text-input-version=3";
+        })
         wechat
       ];
     };
