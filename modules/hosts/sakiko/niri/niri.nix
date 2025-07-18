cd957ed3cabdfaff616d9d2eed41be766baa3d29
idkana <ifidkidknow@gmail.com>
2025-07-18 09:40:44 +0800

    add polkit agent (idkana@sakiko)

--- modules/hosts/sakiko/niri/niri.nix
+++ modules/hosts/sakiko/niri/niri.nix
@@ -23,5 +23,23 @@
         rofi-wayland
         nautilus # https://github.com/YaLTeR/niri/issues/702
       ];
+
+      systemd.user.services.polkit-mate-authentication-agent-1 = {
+        Unit = {
+          Description = "polkit-mate-authentication-agent-1";
+          Wants = [ "graphical-session.target" ];
+          After = [ "graphical-session.target" ];
+        };
+        Install = {
+          WantedBy = [ "graphical-session.target" ];
+        };
+        Service = {
+          Type = "simple";
+          ExecStart = "${pkgs.mate.mate-polkit}/libexec/polkit-mate-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+      };
     };
 }
