ac193eaeb0d15895fa457303ebd7320126e97e89
idkana <ifidkidknow@gmail.com>
2025-07-27 00:21:03 +0800

    nix flake update

--- modules/hosts/sakiko/idkana/home.nix
+++ modules/hosts/sakiko/idkana/home.nix
@@ -34,19 +34,7 @@
         zed-editor
         kdePackages.dolphin
         kdePackages.dolphin-plugins
-        (inputs.nixpkgs-master.legacyPackages.x86_64-linux.maa-cli.override {
-          maa-assistant-arknights =
-            inputs.nixpkgs-master.legacyPackages.x86_64-linux.maa-assistant-arknights.overrideAttrs
-              {
-                version = "5.20.0";
-                src = fetchFromGitHub {
-                  owner = "MaaAssistantArknights";
-                  repo = "MaaAssistantArknights";
-                  rev = "v5.20.0";
-                  hash = "sha256-XqzCxzmNJHG+aYBJoeAAryFx3XctrGBk6QqoonCbrWU=";
-                };
-              };
-        })
+        maa-cli
         obsidian
         vesktop
         (vscode.override {
