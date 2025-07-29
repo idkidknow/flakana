6fefe1cc79332ca1570fcb204dc8e926cab1f43c
idkana <ifidkidknow@gmail.com>
2025-07-30 00:32:32 +0800

    make fcitx5 use qt6 (sakiko)

--- modules/hosts/sakiko/fcitx.nix
+++ modules/hosts/sakiko/fcitx.nix
@@ -6,6 +6,7 @@
         enable = true;
         type = "fcitx5";
         fcitx5.waylandFrontend = true;
+        fcitx5.plasma6Support = true;
         fcitx5.addons = with pkgs; [
           kdePackages.fcitx5-chinese-addons
           fcitx5-rime
