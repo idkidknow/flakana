c627c7caabf3a66d2618798a63bdf51402f3ca2c
idkana <ifidkidknow@gmail.com>
2025-08-03 17:46:20 +0800

    fix my gamepad vibrating issues

---
+++ modules/hosts/sakiko/gamepad.nix
@@ -0,0 +1,14 @@
+{
+  flake.modules.nixos."hosts/sakiko" =
+    { pkgs, ... }:
+    {
+      systemd.services.just-cat-gamepad = {
+        description = "My gamepad keeps vibrating if no one is fetching its data and I don't know why.";
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          ExecStart = "${pkgs.uutils-coreutils-noprefix}/bin/cat /dev/input/js0";
+          StandardOutput = "null";
+        };
+      };
+    };
+}
