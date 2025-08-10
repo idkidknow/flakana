358ecb220dcf73cc614eabd6c3da2ce49618531d
idkana <ifidkidknow@gmail.com>
2025-08-10 18:16:44 +0800

    add clipcat (idkana@sakiko)

---
+++ modules/hosts/sakiko/idkana/clipcat.nix
@@ -0,0 +1,38 @@
+{
+  flake.modules.homeManager."idkana@sakiko" =
+    { ... }:
+    {
+      services.clipcat = {
+        enable = true;
+        enableSystemdUnit = true;
+        daemonSettings = {
+          daemonize = true;
+          max_history = 100;
+          log = {
+            emit_journald = true;
+            level = "INFO";
+          };
+          watcher = {
+            enable_clipboard = true;
+            enable_primary = true;
+            capture_image = true;
+          };
+        };
+        ctlSettings = {
+          server_endpoint = "/run/user/1000/clipcat/grpc.sock";
+          preview_length = 100;
+        };
+        menuSettings = {
+          server_endpoint = "/run/user/1000/clipcat/grpc.sock";
+          finder = "rofi";
+          preview_length = 100;
+          rofi = {
+            line_length = 120;
+            menu_length = 25;
+            menu_prompt = "Clipcat";
+            extra_arguments = [];
+          };
+        };
+      };
+    };
+}
