0353114a3efc165c5f768ad3534cd7a8d24970a8
idkana <ifidkidknow@gmail.com>
2025-08-11 19:12:07 +0800

    enable pipewire low latency settings

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -1,13 +1,18 @@
+{ inputs, ... }:
 {
   flake.modules.nixos.common-desktop =
     { ... }:
     {
+      imports = [ inputs.nix-gaming.nixosModules.pipewireLowLatency ];
+
       security.rtkit.enable = true;
       services.pipewire = {
         enable = true;
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
+
+        lowLatency.enable = true;
       };
     };
 
