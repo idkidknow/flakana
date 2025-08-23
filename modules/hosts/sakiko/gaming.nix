96377c8bb58395cd386e27277e41c82c3491e328
idkana <ifidkidknow@gmail.com>
2025-08-23 18:34:27 +0800

    add proton-ge-bin to steam (sakiko)

--- modules/hosts/sakiko/gaming.nix
+++ modules/hosts/sakiko/gaming.nix
@@ -1,13 +1,16 @@
 { inputs, ... }:
 {
   flake.modules.nixos."hosts/sakiko" =
-    { ... }:
+    { pkgs, ... }:
     {
       nixpkgs.config.allowUnfree = true;
       programs.steam = {
         enable = true;
         remotePlay.openFirewall = true;
         dedicatedServer.openFirewall = true;
+        extraCompatPackages = with pkgs; [
+          proton-ge-bin
+        ];
       };
       programs.gamemode.enable = true;
 
