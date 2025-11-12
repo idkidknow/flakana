6abc53639af3487d008fd5fedabc738dba7dd95a
idkana <ifidkidknow@gmail.com>
2025-11-12 23:48:34 +0800

    repeat=false in niri Super+Space binding (idkana@sakiko)

--- modules/hosts/sakiko/idkana/hexecute.nix
+++ modules/hosts/sakiko/idkana/hexecute.nix
@@ -6,6 +6,7 @@
       home.packages = [ inputs.hexecute.packages.x86_64-linux.default ];
 
       programs.niri.settings.binds."Super+Space" = {
+        repeat = false;
         action.spawn = "hexecute";
       };
     };
