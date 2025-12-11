3c05a7be29fd2bd8a993e7f4838a24d80d4e1f14
idkana <ifidkidknow@gmail.com>
2025-12-12 01:13:36 +0800

    unpin nvidia driver (sakiko)

--- modules/hosts/sakiko/nvidia.nix
+++ modules/hosts/sakiko/nvidia.nix
@@ -9,15 +9,15 @@
         open = true;
         powerManagement.enable = true;
 
-        # track: https://forums.developer.nvidia.com/t/580-release-feedback-discussion/341205
-        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          version = "580.82.09";
-          sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
-          sha256_aarch64 = "sha256-6tHiAci9iDTKqKrDIjObeFdtrlEwjxOHJpHfX4GMEGQ=";
-          openSha256 = "sha256-YB+mQD+oEDIIDa+e8KX1/qOlQvZMNKFrI5z3CoVKUjs=";
-          settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
-          persistencedSha256 = "sha256-lbYSa97aZ+k0CISoSxOMLyyMX//Zg2Raym6BC4COipU=";
-        };
+        package = config.boot.kernelPackages.nvidiaPackages.beta;
+        # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        #   version = "580.82.09";
+        #   sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
+        #   sha256_aarch64 = "sha256-6tHiAci9iDTKqKrDIjObeFdtrlEwjxOHJpHfX4GMEGQ=";
+        #   openSha256 = "sha256-YB+mQD+oEDIIDa+e8KX1/qOlQvZMNKFrI5z3CoVKUjs=";
+        #   settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
+        #   persistencedSha256 = "sha256-lbYSa97aZ+k0CISoSxOMLyyMX//Zg2Raym6BC4COipU=";
+        # };
       };
     };
 }
