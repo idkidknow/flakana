263e374f7b4d28957d28f70bf891bfb6acf82bc0
idkana <ifidkidknow@gmail.com>
2025-12-23 00:45:15 +0800

    disable steamux in osu-stable

--- modules/hosts/sakiko/gaming/osu.nix
+++ modules/hosts/sakiko/gaming/osu.nix
@@ -8,21 +8,21 @@
     {
       home.packages = [
         (inputs.nix-gaming.packages.x86_64-linux.osu-stable.override {
-          preCommands = ''
-            unset WINEARCH
-            if [ -e "$WINEPREFIX/steamux.sock" ]; then
-                if [ -z "$1" ]; then
-                    ${steamux}/bin/steamuxctl --socket-path $WINEPREFIX/steamux.sock run -p -- "$OSU"
-                else
-                    ARG1_COMPAT_PATH="$(${steamux}/bin/steamuxctl --socket-path $WINEPREFIX/steamux.sock run -- "$PROTONPATH/proton" getcompatpath "$(realpath "$1")")"
-                    shift
-                    ${steamux}/bin/steamuxctl --socket-path $WINEPREFIX/steamux.sock run -p -- "$OSU" "$ARG1_COMPAT_PATH" "$@"
-                fi
-            else
-                ${pkgs.gamemode}/bin/gamemoderun ${steamux}/bin/steamux umu --socket-path $WINEPREFIX/steamux.sock -- "$OSU" "$@"
-            fi
-            exit
-          '';
+          # preCommands = ''
+          #   unset WINEARCH
+          #   if [ -e "$WINEPREFIX/steamux.sock" ]; then
+          #       if [ -z "$1" ]; then
+          #           ${steamux}/bin/steamuxctl --socket-path $WINEPREFIX/steamux.sock run -p -- "$OSU"
+          #       else
+          #           ARG1_COMPAT_PATH="$(${steamux}/bin/steamuxctl --socket-path $WINEPREFIX/steamux.sock run -- "$PROTONPATH/proton" getcompatpath "$(realpath "$1")")"
+          #           shift
+          #           ${steamux}/bin/steamuxctl --socket-path $WINEPREFIX/steamux.sock run -p -- "$OSU" "$ARG1_COMPAT_PATH" "$@"
+          #       fi
+          #   else
+          #       ${pkgs.gamemode}/bin/gamemoderun ${steamux}/bin/steamux umu --socket-path $WINEPREFIX/steamux.sock -- "$OSU" "$@"
+          #   fi
+          #   exit
+          # '';
         })
         inputs.nix-gaming.packages.x86_64-linux.osu-lazer-bin
       ];
