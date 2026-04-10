925b9cbd98e25fa5ea00518c351ba19d1f74fec8
idkana <ifidkidknow@gmail.com>
2026-04-10 19:11:17 +0800

    nix flake update

--- modules/hosts/sakiko/ntfs.nix
+++ modules/hosts/sakiko/ntfs.nix
@@ -6,18 +6,22 @@
         "/mnt/c" = {
           device = "/dev/disk/by-uuid/2C8851298850F336";
           options = [ "nofail" ];
+          fsType = "ntfs3";
         };
         "/mnt/d" = {
           device = "/dev/disk/by-uuid/6AA86BF5A86BBDE7";
           options = [ "nofail" ];
+          fsType = "ntfs3";
         };
         "/mnt/e" = {
           device = "/dev/disk/by-uuid/A8CC8232CC81FB38";
           options = [ "nofail" ];
+          fsType = "ntfs3";
         };
         "/mnt/f" = {
           device = "/dev/disk/by-uuid/E24E1E354E1E02C9";
           options = [ "nofail" ];
+          fsType = "ntfs3";
         };
       };
     };
