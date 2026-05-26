3a06101f68318ffb25556484b166eae4e0c793ea
idkana <ifidkidknow@gmail.com>
2026-05-27 04:44:21 +0800

    add open-orpheus (idkana@sakiko)

---
+++ modules/hosts/sakiko/idkana/open-orpheus.nix
@@ -0,0 +1,88 @@
+{ self, ... }:
+{
+  flake.modules.homeManager."idkana@sakiko" =
+    { pkgs, ... }:
+    {
+      home.packages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.open-orpheus ];
+    };
+
+  perSystem =
+    { pkgs, ... }:
+    let
+      inherit (pkgs)
+        lib
+        stdenv
+        fetchurl
+        binutils
+        zstd
+        patchelf
+        ;
+    in
+    {
+      packages.open-orpheus = stdenv.mkDerivation rec {
+        pname = "open-orpheus";
+        version = "0.12.2";
+        src = fetchurl {
+          url = "https://github.com/YUCLing/open-orpheus/releases/download/v${version}/open-orpheus_${version}_amd64.deb";
+          hash = "sha256-wmstKySkQnaO+UTBRNMdTfnetuQ/Bt/VseXmk6aVfoI=";
+        };
+
+        nativeBuildInputs = [
+          binutils
+          zstd
+          patchelf
+        ];
+
+        buildInputs = with pkgs; [
+          gtk3
+          libnotify
+          nss
+          xdg-utils
+          at-spi2-core
+          libdrm
+          mesa
+          libxcb
+          glib
+          nspr
+          cups
+          dbus
+          cairo
+          pango
+          libX11
+          libXcomposite
+          libXdamage
+          libXext
+          libXfixes
+          libXrandr
+          libgbm
+          expat
+          libxkbcommon
+          udev
+          alsa-lib
+        ];
+
+        unpackPhase = ''
+          runHook preUnpack
+
+          ar vx $src
+          tar -xvf data.tar.zst
+
+          runHook postUnpack
+        '';
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/bin
+          cp -r usr/share $out/
+          patchelf \
+            --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+            --set-rpath "${lib.makeLibraryPath buildInputs}:$out/lib/open-orpheus" \
+            usr/lib/open-orpheus/open-orpheus
+          cp -r usr/lib $out/
+          ln -s $out/lib/open-orpheus/open-orpheus $out/bin/open-orpheus
+
+          runHook postInstall
+        '';
+      };
+    };
+}
