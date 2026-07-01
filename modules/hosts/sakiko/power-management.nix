00eba3b3e6cffe7b68d5f3ca5e6594fed6217a45
idkana <ifidkidknow@gmail.com>
2026-07-01 14:51:42 +0800

    sakiko: disable mouse wake up

--- modules/hosts/sakiko/power-management.nix
+++ modules/hosts/sakiko/power-management.nix
@@ -5,5 +5,10 @@
       powerManagement.enable = true;
       services.power-profiles-daemon.enable = true;
       services.upower.enable = true;
+
+      # Razer mouse
+      services.udev.extraRules = ''
+        ACTION=="add|change", SUBSYSTEM=="usb", DRIVERS=="usb", ATTR{idVendor}=="1532", ATTR{idProduct}=="0084", ATTR{power/wakeup}="disabled"
+      '';
     };
 }
