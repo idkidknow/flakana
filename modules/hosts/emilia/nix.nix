c1f7281c95fb33bead774874f10abfad441cebc4
idkana <ifidkidknow@gmail.com>
2026-08-10 12:03:36 +0000

    emilia: disable nix sandbox
    [skip ci]

--- modules/hosts/emilia/nix.nix
+++ modules/hosts/emilia/nix.nix
@@ -3,6 +3,7 @@
     { ... }:
     {
       nix.access-tokens-file = "/home/idkana/nix-access-tokens";
+      nix.settings.sandbox = false;
       nix.named-substituters = {
         idkidknow.enable = true;
         numtide.enable = true;
