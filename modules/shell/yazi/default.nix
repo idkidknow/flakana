6c0c04ed5e26027f95a04ff9bc6e612be20c063c
idkana <ifidkidknow@gmail.com>
2026-08-11 02:17:09 +0000

    update yazi config

--- modules/shell/yazi/default.nix
+++ modules/shell/yazi/default.nix
@@ -27,13 +27,13 @@
     {
       packages.yazi-flavor-kanagawa = pkgs.stdenv.mkDerivation {
         pname = "yazi-flavor-kanagawa";
-        version = "0-unstable-2025-02-23";
+        version = "0-unstable-2026-08-11";
 
         src = pkgs.fetchFromGitHub {
           owner = "dangooddd";
           repo = "kanagawa.yazi";
-          rev = "31167ed54c9cc935b2fa448d64d367b1e5a1105d";
-          hash = "sha256-phwGd1i/n0mZH/7Ukf1FXwVgYRbXQEWlNRPCrmR5uNk=";
+          rev = "04985d12842b06bdb3ad5f1b3d7abc631059b7f5";
+          hash = "sha256-Yz0zRVzmgbrk0m7OkItxIK6W0WkPze/t09pWFgziNrw=";
         };
 
         buildPhase = ''
