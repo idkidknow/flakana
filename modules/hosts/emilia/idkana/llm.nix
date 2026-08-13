0891f750f6b5957f2ca634158a3c5c34009bc753
idkana <ifidkidknow@gmail.com>
2026-08-13 00:52:59 +0000

    emilia/idkana: fix pi

--- modules/hosts/emilia/idkana/llm.nix
+++ modules/hosts/emilia/idkana/llm.nix
@@ -7,7 +7,17 @@
         unset __ETC_PROFILE_NIX_SOURCED
         exec -a codex ${inputs.llm-agents.packages.aarch64-linux.codex}/bin/codex "$@"
       '';
-      pi = inputs.llm-agents.packages.aarch64-linux.pi;
+      pi-unwrapped = inputs.llm-agents.packages.aarch64-linux.pi;
+      pi = pkgs.writeShellScriptBin "pi" ''
+        export PI_PACKAGE_DIR=${pi-unwrapped}/libexec/pi
+        export PI_SKIP_VERSION_CHECK=1
+        export PI_TELEMETRY=0
+
+        exec ${pkgs.stdenv.cc.bintools.dynamicLinker} \
+          --argv0 pi \
+          ${pi-unwrapped}/libexec/pi/pi \
+          "$@"
+      '';
     in
     {
       home.packages = [
