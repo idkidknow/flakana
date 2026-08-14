979b9497b0483ed8de7231ea08a1013be99bfb52
idkana <ifidkidknow@gmail.com>
2026-08-15 03:19:49 +0800

    nix flake update

--- modules/hosts/emilia/system.nix
+++ modules/hosts/emilia/system.nix
@@ -4,46 +4,6 @@
   self,
   ...
 }:
-let
-  # new nixpkgs has moved `nix.enable` and `nix.package`
-  # from `nix-daemon.nix` to `config/nix.nix`
-  # and started referencing `programs.bash.completion.enable`
-  # and `services.displayManager.hiddenUsers`
-  # track: ?
-  system-manager-nix-nix-patched =
-    {
-      config,
-      lib,
-      nixosModulesPath,
-      ...
-    }:
-    {
-      options = {
-        programs.bash.completion.enable = lib.mkOption {
-          type = lib.types.bool;
-          default = false;
-        };
-      };
-
-      imports = [
-        (nixosModulesPath + "/services/display-managers/default.nix")
-        (nixosModulesPath + "/services/display-managers/generic.nix")
-        {
-          disabledModules = [
-            "${inputs.system-manager.outPath}/nix/modules/upstream/nixpkgs/nix.nix"
-          ];
-        }
-      ];
-
-      config = lib.mkIf config.nix.enable {
-        environment.etc."nix/nix.conf".replaceExisting = true;
-        nix.settings.experimental-features = lib.mkDefault [
-          "nix-command"
-          "flakes"
-        ];
-      };
-    };
-in
 {
   flake.modules.systemManager."hosts/emilia" =
     { pkgs, ... }:
@@ -52,7 +12,6 @@ in
 
       imports = [
         config.flake.modules.systemManager.common
-        system-manager-nix-nix-patched
       ];
 
       environment.systemPackages = with pkgs; [
