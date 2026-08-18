fd579792e49a0d062ec53c2891712a64c4e86f4c
idkana <ifidkidknow@gmail.com>
2026-08-18 02:35:02 +0000

    emilia: manage build users

--- modules/hosts/emilia/nix.nix
+++ modules/hosts/emilia/nix.nix
@@ -4,6 +4,7 @@
     {
       nix.access-tokens-file = "/home/idkana/nix-access-tokens";
       nix.settings.sandbox = false;
+      nix.nrBuildUsers = 32;
       nix.named-substituters = {
         idkidknow.enable = true;
         numtide.enable = true;
