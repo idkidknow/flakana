73543351237c15551c9f1094958ab488d2ca4d8a
idkana <ifidkidknow@gmail.com>
2026-08-23 02:02:42 +0800

    nix flake update

--- modules/hosts/sakiko/incus.nix
+++ modules/hosts/sakiko/incus.nix
@@ -11,18 +11,5 @@
       daeLanInterfaces = [ "incusbr0" ];
 
       users.users.idkana.extraGroups = [ "incus-admin" ];
-
-      # track: https://github.com/nikstur/userborn/issues/7
-      environment.etc = {
-        "subuid" = {
-          text = "root:1000000:1000000000\n";
-          mode = "0444";
-        };
-
-        "subgid" = {
-          text = "root:1000000:1000000000\n";
-          mode = "0444";
-        };
-      };
     };
 }
--- modules/hosts/sakiko/scheduler.nix
+++ modules/hosts/sakiko/scheduler.nix
@@ -1,22 +1,10 @@
 {
   flake.modules.nixos."hosts/sakiko" =
-    { pkgs, ... }:
-    let
-      # https://github.com/sched-ext/scx/pull/3721
-      # track: wait for https://github.com/sched-ext/scx/releases
-      patchedRustscheds = pkgs.scx.rustscheds.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [ ./scx-rusty-retained-cpumask.patch ];
-      });
-    in
+    { ... }:
     {
       services.scx = {
         enable = true;
         scheduler = "scx_rusty";
-        package = pkgs.scx.full.override {
-          scx = pkgs.scx // {
-            rustscheds = patchedRustscheds;
-          };
-        };
       };
     };
 }
