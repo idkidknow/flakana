1692fcfcf232c667e77553773331c89a83f9146d
idkana <ifidkidknow@gmail.com>
2026-09-18 17:24:27 +0800

    tomori: add authorized keys

--- modules/hosts/tomori/users.nix
+++ modules/hosts/tomori/users.nix
@@ -7,11 +7,13 @@
         extraGroups = [ "wheel" ];
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ7q2G+4ZERwCOkI2LKv07NDfPofuYPVFIU2q7tru8y7"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBHx2DAGN1ZSTu3RKdAQWwGoXzI+ijl6JJTO9vH3pdPd"
         ];
       };
 
       users.users.root.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ7q2G+4ZERwCOkI2LKv07NDfPofuYPVFIU2q7tru8y7"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBHx2DAGN1ZSTu3RKdAQWwGoXzI+ijl6JJTO9vH3pdPd"
       ];
     };
 }
